20f67d3bb5e6aae3f2b390c00e06b9daa589d480
VirgillvanderMeer <vsmvandermeer@gmail.com>
2025-02-28 18:51:52 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -6,8 +6,8 @@ st.title("LegalCheck: De Briefhulp ⚖️✍️📜")
 st.write("Genereer eenvoudig de Beslissing op bezwaar. 🔄🤝🚀")
 
 naam = st.text_area("✅ Typ of plak hier de Waarneming van de verballisant", height=200)
-
 naam = st.text_area("✅ Typ of plak hier het Bezwaarschrift", height=200)
+naam = st.text_area("✅ Typ hier de besproken punten in de telefonische hoorzitting", height=200)
 
 # Sidebar (left)
 with st.sidebar:
