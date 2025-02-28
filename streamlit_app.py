07c4fa3bbc8cb421bb72e2cfdd18187d70c4f37e
VirgillvanderMeer <vsmvandermeer@gmail.com>
2025-02-28 19:20:28 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -11,7 +11,7 @@ uploaded_file = st.file_uploader("Upload een afbeelding", type=["jpg", "jpeg", "
 
 if uploaded_file is not None:
     image = Image.open(uploaded_file)
-    st.image(image, caption="Geüploade afbeelding.", use_column_width=True)
+    st.image(image, caption="Geüploade afbeelding.")
 
 verballisant = st.text_area("✅ Typ of plak hier de Waarneming van de verballisant", height=200)
 bezwaarschrift = st.text_area("✅ Typ of plak hier het Bezwaarschrift", height=200)
